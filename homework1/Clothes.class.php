03657044d5bf3493b05b31cda3c305dc3f47f2d6
Olesya090585 <okochman@ya.ru>
2019-03-24 10:55:10 +0300

    Homework 1

--- homework1/Clothes.class.php
+++ homework1/Clothes.class.php
@@ -1,7 +1,87 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: o.varnakova
- * Date: 2019-03-23
- * Time: 11:00
- */
+
+class Clothes
+{
+    private $name;
+    private $price;
+    private $color;
+    private $size;
+    private $material;
+
+    function __construct($name, $price, $color, $size, $material)
+    {
+        $this->setName($name);
+        $this->setPrice($price);
+        $this->setColor($color);
+        $this->setSize($size);
+        $this->setMaterial($material);
+//        $this->display();
+    }
+
+//  SETTER
+
+    function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    function setSize($size)
+    {
+        $this->size = $size;
+    }
+
+    function setMaterial($material)
+    {
+        $this->material = $material;
+    }
+
+//    GETTER
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function getPrice()
+    {
+        return $this->price;
+    }
+
+    function getColor()
+    {
+        return $this->color;
+    }
+
+    function getSize()
+    {
+        return $this->size;
+    }
+
+    function getMaterial()
+    {
+        return $this->material;
+    }
+
+    function display()
+    {
+        echo "Товар: " . $this->getName() . ";" . " Цвет: " . $this->getColor() . ";" . " Размер: " . $this->getSize() . ";" .
+            " Материал: " . $this->getMaterial() . ";" . " Цена: " . $this->getPrice() . "<br>";
+    }
+}
+
+$clothes1 = new Clothes('Джинсы', 2500, 'черные', 42, 'хлопок');
+$clothes1->display();
+$clothes2 = new Clothes('Юбка', 1500, 'синяя', 42, 'хлопок');
+$clothes2->display();
+$clothes3 = new Clothes('Футболка', 500, 'белая', 40, 'хлопок');
+$clothes3->display();
--- homework1/SpecialClothes.class.php
+++ homework1/SpecialClothes.class.php
@@ -1,7 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: o.varnakova
- * Date: 2019-03-23
- * Time: 16:56
- */
+include "Clothes.class.php";
+
+class SpecialClothes extends Clothes
+{
+    private $category;
+
+    public function __construct($name, $price, $color, $size, $material, $category)
+    {
+        parent::__construct($name, $price, $color, $size, $material);
+        $this->category = $category;
+
+    }
+
+    function setCategory($category)
+    {
+        $this->category = $category;
+    }
+
+    function getCategory()
+    {
+        return $this->category;
+    }
+
+    function display()
+    {
+//        parent::display();
+//        echo " Категория: " . $this->getCategory();
+        echo "Товар: " . $this->getName() . ";" . " Цвет: " . $this->getColor() . ";" . " Размер: " . $this->getSize() . ";" .
+            " Материал: " . $this->getMaterial() . ";" . " Цена: " . $this->getPrice() ." Категория: " . $this->getCategory(). "<br>";
+    }
+
+}
+
+$sc1 = new SpecialClothes('Униформа', 5500, 'белый', 42, 'хлопок', 'Медицина');
+$sc1->display();
